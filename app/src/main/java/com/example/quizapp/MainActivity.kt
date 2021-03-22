30ef4f80fdcc4a122c61775c140651419a99fcd5
Aleksa <zabumaphu@gmail.com>
2021-03-22 01:50:04 -0400

    Toast added.

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        var btn_start = this.findViewById<Button>(R.id.btn_start)
+        var et_name = this.findViewById<TextView>(R.id.et_name)
+        btn_start.setOnClickListener{
+            if(et_name.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name!", Toast.LENGTH_SHORT).show()
+            }
+            }
+        }
     }
-}
